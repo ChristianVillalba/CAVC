716826e3f66f80cd7441701676bc418acf215bdb
Christian Villalba UK <christianvillalba.uk@gmail.com>
2025-06-06 00:09:37 +0100

    working in aquarium.html file

---
+++ Python/practice.py
@@ -0,0 +1,7 @@
+age = input("what is your age?:")
+age_int = int(age)
+if age_int >= 35:
+    print("You are old enough to be a president")
+else:
+    print("You are too young to be a president")
+print("Have a good day")
