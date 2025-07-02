515cb6bb5cad6fec7c469c50b69c8600ff8fcf8f
Christian Villalba UK <christianvillalba.uk@gmail.com>
2025-07-02 12:46:19 +0100

    adds file: basic functions

---
+++ Python/basicFunctions.py
@@ -0,0 +1,37 @@
+rounded = round(3.1415) 
+twoDecimals = round(3.1415, 2)
+print("round() examples:")
+print(rounded)   # rounds to the nearest whole number 
+print(twoDecimals) # rounds to the specified(2) number of decimal
+
+print("len() examples:")
+print(len("Hello World")) #returns the number of items in a string or list
+print(len([ "banana", "orange" ]))
+
+print("range() examples:")
+for i in range(1, 6): # generates a sequence of numbers
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def helloWorld():   # we define the function
+# 	print("Hello World!")
+# helloWorld()    # we call the function
+
+
+# import math
+# import random
+# print(math.trunc(5.9))  # Output: 5  (removes decimals)
+# print(random.random()) # prints a random float between 0.0 and 1.0
+# print(random.randint(1, 6))  # Print a random number between 1 and 6
+
